3bd7fa0b9610e6c4902928f19441e2b3461c7fff
HAMZA-CH <hakram554r@gmail.com>
2024-12-17 16:31:27 +0500

    Ninth Commit

--- lib/features/ShiftScreen/ShiftDetail/Shift_Detail.dart
+++ lib/features/ShiftScreen/ShiftDetail/Shift_Detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shifts_management/UiHelpers/theme/Color_Palate.dart';
 import 'package:shifts_management/UiHelpers/widgets/Custom_Banner.dart';
 import 'package:shifts_management/features/ShiftScreen/ShiftDetail/Widgets/Custom_Time_Widget.dart';
 import 'package:shifts_management/features/ShiftScreen/widgets/ShiftWidget/Status_of_Shift.dart';
@@ -9,35 +10,71 @@ class ShiftDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Shift Details"),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(FontAwesomeIcons.ellipsisVertical),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomBanner(bannerTitle: "Bars & Waiters"),
-                StatusOfShift(),
-              ],
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Shift Details"),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(FontAwesomeIcons.ellipsisVertical),
+            )
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(180),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomBanner(bannerTitle: "Bars & Waiters"),
+                      const StatusOfShift(),
+                    ],
+                  ),
+                  const CustomTimeWidget(),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const TabBar(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      labelColor: Palate.primaryColor,
+                      unselectedLabelColor: Palate.TextFieldTextColor,
+                      overlayColor: WidgetStateColor.transparent,
+                      indicatorSize: TabBarIndicatorSize.tab,
+                      dividerHeight: 2,
+                      unselectedLabelStyle: TextStyle(color: Color(0XFF526070)),
+                      labelStyle:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                      indicatorColor: Palate.primaryColor,
+                      dividerColor: Color(0XFFD2E4FF),
+                      tabs: [
+                        Tab(text: "Details"),
+                        Tab(text: "Activities"),
+                      ]),
+                ],
+              ),
             ),
-            CustomTimeWidget(),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Tab(
+              text: "Details",
+            ),
+            Tab(
+              text: "Activities",
+            ),
           ],
         ),
       ),
