90d079b8dcfb3feaeb51636bdaa2b9e2fdb8a4bb
HAMZA-CH <hakram554r@gmail.com>
2025-06-07 20:59:30 +0500

    minor changes

--- lib/features/ShiftScreen/ShiftDetail/DetailsScreen/Widgets/Assigned_Widget.dart
+++ lib/features/ShiftScreen/ShiftDetail/DetailsScreen/Widgets/Assigned_Widget.dart
@@ -7,7 +7,7 @@ class AssignedWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       height: 40,
       width: 370,
       decoration: BoxDecoration(
